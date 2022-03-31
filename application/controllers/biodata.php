62f307bf5ff5cbf99a08ba187db94627fe409f7f
Nawawi28 <nawaw948@gmail.com>
2022-03-31 22:57:10 +0700

    tugas wp 2

---
+++ application/controllers/biodata.php
@@ -0,0 +1,12 @@
+<?php
+class biodata extends CI_Controller
+{
+    public function index()
+{
+echo"<h1>perkenalkan</h1>";
+echo"Nama saya asyik Nawawi
+ saya tinggal di daerah jakarta timur
+ olah raga yang saya sukai adalah bulutangkis";
+
+}
+}
